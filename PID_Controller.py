d1874cea917c7c4590168fb9792e7957521cc726
Andrey <90500480+SorokinAS@users.noreply.github.com>
2022-12-25 16:17:23 +0300

    Update PID_Controller.py

--- PID_Controller.py
+++ PID_Controller.py
@@ -6,21 +6,21 @@ error = []
 for i in range(2):
     error.append(0)
 
-def addValue(value, mas):
+def add_value(value, mas):
     mas.append(value)
     if len(mas) > 2:
         mas.pop(0)
     return mas
 
-kp = 0.04
-ki = 0.00001
-kd = 0.9
-set_point = 500
+KP = 0.04
+KI = 0.00001
+KD = 0.9
+SET_POINT = 500
 
-Port = 9031
-Host = 'localhost'
-Port_ctrl = 9101
-Port_sp = 9111
+PORT = 9031
+HOST = 'localhost'
+PORT_CTRL = 9101
+PORT_SP = 9111
 
 s = socket(AF_INET, SOCK_DGRAM)
 s.bind((Host, Port))
@@ -28,10 +28,10 @@ sock = socket(AF_INET, SOCK_DGRAM)
 while True:
     data, addr = s.recvfrom(1024)
     data = struct.unpack('d', data)[0]
-    error = addValue(set_point - data, error)
-    pid_ctrl = kp * error[1] + ki * (reg+error[1]) + kd * (error[1]-error[0])
+    error = add_value(SET_POINT - data, error)
+    pid_ctrl = KP * error[1] + KI * (reg+error[1]) + KD * (error[1]-error[0])
     reg = reg + error[1]
     mes = struct.pack('d', pid_ctrl)
-    sock.sendto(mes, (Host, Port_ctrl))
-    mes_sp = struct.pack('d', set_point)
+    sock.sendto(mes, (HOST, PORT_CTRL))
+    mes_sp = struct.pack('d', SET_POINT)
     sock.sendto(mes_sp, (Host, Port_sp))
